b212d9ad6087b69cb6472202b52b22dfdb6a9e6a
Simone Alberto Peirone <simone.peirone@polito.it>
2025-07-16 22:17:08 +0200

    Add scripts

---
+++ scripts/lta.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=timechat_lta                     # Job name
+#SBATCH --output=logs/timechat/%x_%j.out            # Output log (%x=job-name, %j=job-id)
+#SBATCH --error=logs/timechat/%x_%j.err             # Error log
+#SBATCH --ntasks=1                                  # Number of tasks
+#SBATCH --cpus-per-task=24                          # Number of CPU cores per task
+#SBATCH --mem=32G                                   # Total memory
+#SBATCH --gres=gpu:1                                # Number of GPUs (remove if not needed)
+#SBATCH --time=24:00:00                             # Time limit (hh:mm:ss)
+#SBATCH --partition=fair_gpu                        
+
+conda activate timechat
+
+# Run the script
+
+echo "Running with 8 frames and 0 icl examples..."
+python -m eval_lta --num-frames 8
+
+echo "Running with 16 frames and 0 icl examples..."
+python -m eval_lta --num-frames 16
+
+echo "Running with 32 frames and 0 icl examples..."
+python -m eval_lta --num-frames 32
---
+++ scripts/oscc_icl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=timechat_oscc_icl                # Job name
+#SBATCH --output=logs/timechat/%x_%j.out            # Output log (%x=job-name, %j=job-id)
+#SBATCH --error=logs/timechat/%x_%j.err             # Error log
+#SBATCH --ntasks=1                                  # Number of tasks
+#SBATCH --cpus-per-task=24                          # Number of CPU cores per task
+#SBATCH --mem=32G                                   # Total memory
+#SBATCH --gres=gpu:1                                # Number of GPUs (remove if not needed)
+#SBATCH --time=24:00:00                             # Time limit (hh:mm:ss)
+#SBATCH --partition=fair_gpu                        
+
+conda activate timechat
+
+echo "Running with 8 frames and 3 icl examples..."
+python -m eval_oscc --icl-examples 3 --num-frames 8
