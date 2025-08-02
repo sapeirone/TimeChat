2c215ded385d74660a8883b21f33829a9681937e
Simone Alberto Peirone <simone.peirone@polito.it>
2025-08-02 19:26:37 +0200

    Add experiments with 96 input frames

---
+++ scripts/96f/lta.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=timechat_lta_ZS                     # Job name
+#SBATCH --output=logs/timechat/96f/%x_%j.out            # Output log (%x=job-name, %j=job-id)
+#SBATCH --error=logs/timechat/96f/%x_%j.err             # Error log
+#SBATCH --ntasks=1                                  # Number of tasks
+#SBATCH --cpus-per-task=24                          # Number of CPU cores per task
+#SBATCH --mem=32G                                   # Total memory
+#SBATCH --gres=gpu:1                                # Number of GPUs (remove if not needed)
+#SBATCH --time=24:00:00                             # Time limit (hh:mm:ss)
+#SBATCH --partition=fair_gpu                        
+
+source ~/.bashrc
+conda init
+conda activate timechat
+
+echo "Running with 96 frames..."
+python -m eval_lta --num-frames 96 --video-path /beegfs-scratch/speirone/ego4d_videos/lta --ann-path ego4d/annotations/v1/
